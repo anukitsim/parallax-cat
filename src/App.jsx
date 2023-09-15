b6bbc1f7c507d342ec9673c5e38979c66b6adee6
anukitsim <104069419+anukitsim@users.noreply.github.com>
2023-09-15 23:35:08 +0400

    updated, mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,29 @@ import moon from "./moon.png";
 import land from "./land.png";
 import cat from "./cat.gif";
 
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 import { Parallax, ParallaxLayer, parallaxLayer } from "@react-spring/parallax";
 
 const App = () => {
+  const [isMobile, setIsMobile] = useState(false);
 
   const ref = useRef();
+
+  const checkIsMobile = () => {
+    setIsMobile(window.innerWidth <= 1000);
+  };
+
+  useEffect(() => {
+    checkIsMobile();
+    window.addEventListener("resize", checkIsMobile);
+    return () => {
+      window.removeEventListener("resize", checkIsMobile);
+    };
+  }, []);
+
+
+
 
   return (
     <div>
@@ -20,7 +36,7 @@ const App = () => {
           factor={2}
           style={{
             backgroundImage: `url(${moon})`,
-            backgroundSize: "cover",
+            backgroundSize: isMobile ? "contain" : "cover",
           }}
         />
 
@@ -33,13 +49,26 @@ const App = () => {
             backgroundSize: "cover",
           }}
         ></ParallaxLayer>
-        <ParallaxLayer offset={0.2} speed={0.05} sticky={{start: 0.1, end:3.5}}
-        onClick={() => ref.current.scrollTo(3)}
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          sticky={{ start: 0.1, end: 3.5 }}
+          onClick={() => ref.current.scrollTo(3)}
         >
-          <img src={cat} alt="cat/gif" />
-          
+          <img
+            src={cat}
+            alt="cat/gif"
+            style={{
+              maxWidth: "100%",
+              width: isMobile ? "50%" : "auto",
+            }}
+          />
         </ParallaxLayer>
-        <ParallaxLayer offset={3.2} speed={2} onClick={() => ref.current.scrollTo(3)}>
+        <ParallaxLayer
+          offset={3.2}
+          speed={2}
+          onClick={() => ref.current.scrollTo(3)}
+        >
           <h1>MEOW MEOW!</h1>
         </ParallaxLayer>
       </Parallax>
